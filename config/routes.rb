a9907ac87deebc60be3692e346193549d21c7319
takamasa suzuki <midgard10257@gmail.com>
2017-10-15 11:06:59 +0900

    Add pay jp

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products do
+    post :pay, on: :member
+  end
 end
